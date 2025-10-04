1162b66f3d56a307ec4c573528bd3524a1ea84f6
vasilev202 <vasilevea@mer.ci.nsu.ru>
2025-10-05 01:05:15 +0700

    Add files via upload

---
+++ Васильев2307СВ1/src/components/layout/Menu/Menu.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import styles from './Menu.module.scss';
+
+const Menu = (): React.ReactElement => {
+  const pathname = usePathname ();
+  return (
+    <nav className={styles.Menu}>
+      <div className={pathname === '/' ? styles.linkActive : ''}>
+        <Link href="/">Главная</Link>
+      </div>
+      <div className={pathname === '/groups' ? styles.linkActive : ''}>
+        <Link href="/groups">Группы</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
